fba7c49e1cfa3e9c52fa45ba027738cfcb9b5ed6
Akshata Shanbhag <akshatahan@gmail.com>
2012-05-03 14:27:30 -0500

    Client and client proxy

---
+++ SmartClient/Information.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace SmartClient
+{
+	partial class frmInformation
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+		private System.Windows.Forms.MainMenu mainMenu1;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.mainMenu1 = new System.Windows.Forms.MainMenu();
+			this.cboUserId = new System.Windows.Forms.ComboBox();
+			this.txtUserInformation = new System.Windows.Forms.TextBox();
+			this.lblDataReceived = new System.Windows.Forms.Label();
+			this.lblDataSent = new System.Windows.Forms.Label();
+			this.btnServerCall = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// cboUserId
+			// 
+			this.cboUserId.Items.Add("1");
+			this.cboUserId.Items.Add("2");
+			this.cboUserId.Location = new System.Drawing.Point(21, 40);
+			this.cboUserId.Name = "cboUserId";
+			this.cboUserId.Size = new System.Drawing.Size(100, 22);
+			this.cboUserId.TabIndex = 13;
+			// 
+			// txtUserInformation
+			// 
+			this.txtUserInformation.Location = new System.Drawing.Point(21, 94);
+			this.txtUserInformation.Name = "txtUserInformation";
+			this.txtUserInformation.Size = new System.Drawing.Size(198, 21);
+			this.txtUserInformation.TabIndex = 12;
+			// 
+			// lblDataReceived
+			// 
+			this.lblDataReceived.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.lblDataReceived.Location = new System.Drawing.Point(21, 73);
+			this.lblDataReceived.Name = "lblDataReceived";
+			this.lblDataReceived.Size = new System.Drawing.Size(160, 18);
+			this.lblDataReceived.Text = "Data received from server:";
+			// 
+			// lblDataSent
+			// 
+			this.lblDataSent.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+			this.lblDataSent.Location = new System.Drawing.Point(21, 19);
+			this.lblDataSent.Name = "lblDataSent";
+			this.lblDataSent.Size = new System.Drawing.Size(118, 18);
+			this.lblDataSent.Text = "Data sent to server:";
+			// 
+			// btnServerCall
+			// 
+			this.btnServerCall.Location = new System.Drawing.Point(21, 150);
+			this.btnServerCall.Name = "btnServerCall";
+			this.btnServerCall.Size = new System.Drawing.Size(132, 25);
+			this.btnServerCall.TabIndex = 11;
+			this.btnServerCall.Text = "Call a service.";
+			this.btnServerCall.Click += new System.EventHandler(this.btnServerCall_Click);
+			// 
+			// frmInformation
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(96F, 96F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Dpi;
+			this.AutoScroll = true;
+			this.ClientSize = new System.Drawing.Size(240, 268);
+			this.Controls.Add(this.cboUserId);
+			this.Controls.Add(this.txtUserInformation);
+			this.Controls.Add(this.lblDataReceived);
+			this.Controls.Add(this.lblDataSent);
+			this.Controls.Add(this.btnServerCall);
+			this.Menu = this.mainMenu1;
+			this.Name = "frmInformation";
+			this.Text = "Information";
+			this.Load += new System.EventHandler(this.frmInformation_Load);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		internal System.Windows.Forms.ComboBox cboUserId;
+		internal System.Windows.Forms.TextBox txtUserInformation;
+		internal System.Windows.Forms.Label lblDataReceived;
+		internal System.Windows.Forms.Label lblDataSent;
+		internal System.Windows.Forms.Button btnServerCall;
+	}
+}
+
---
+++ SmartClient/Information.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Xml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.Collections.Specialized;
+using System.IO;
+using MyNameSpace.Screens;
+
+namespace SmartClient
+{
+	public partial class frmInformation : Form
+	{
+		private BasicHttpBinding binding = new BasicHttpBinding();
+		private NameValueCollection appSettings;
+		private EndpointAddress endPointAddress;
+		private InformationServiceClient informationServiceClient;
+	
+		public frmInformation()
+		{
+			InitializeComponent();
+		}
+
+		private void btnServerCall_Click(object sender, EventArgs e)
+		{
+		informationServiceClient = new InformationServiceClient(binding, endPointAddress);
+		UserInformation user = informationServiceClient.GetUserInfo(Convert.ToInt32 (this.cboUserId.Text));
+
+		StringBuilder sb = new StringBuilder();
+			if (user !=null) {
+				if (user.UserName !=null){
+					sb.Append(user.UserName);
+						sb.Append(",");
+				}
+				if (user.Age !=null){
+					sb.Append(user.Age);
+				}
+				this.txtUserInformation.Text = sb.ToString();
+			}
+			else{
+				this.txtUserInformation.Text = "No user information returned.";
+			}
+		}
+
+		private void frmInformation_Load(object sender, System.EventArgs e){
+			//read the configuration settings.There is no ConfigurationManager inbuilt class in CF.
+			// Load config file as an XmlDocument
+			XmlDocument myXmlDocument = new XmlDocument();
+			myXmlDocument.Load(String.Format("{0}.config", System.Reflection.Assembly.GetExecutingAssembly().GetName().CodeBase));
+
+			appSettings = new NameValueCollection();
+			//Read appsettings
+			foreach (XmlNode appSettingNode in myXmlDocument.SelectNodes("/configuration/appSettings/add"))
+			{
+				appSettings.Add(appSettingNode.Attributes[@"key"].Value, appSettingNode.Attributes[@"value"].Value);
+			}
+
+			endPointAddress = new EndpointAddress(Path.Combine(appSettings[@"HostWebSite"], appSettings[@"InformationService"]));
+		}
+
+	}
+}
