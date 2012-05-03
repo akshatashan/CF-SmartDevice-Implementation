a6fdd4e51dd0d61ce554764e5905e02f437cc10d
Akshata Shanbhag <akshatahan@gmail.com>
2012-05-03 12:57:55 -0500

    website and controller commits.

---
+++ SmartController/InformationService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+
+namespace MyNameSpace
+{
+	[ServiceContract(Namespace ="http://schemas.a-t-g.com/2010/08/web/services", ConfigurationName="InformationScreenService")] 
+	[AspNetCompatibilityRequirements(RequirementsMode=AspNetCompatibilityRequirementsMode.Required)] 
+	public class InformationService
+	{
+		[OperationContract]
+		public  UserInformation GetUserInfo(int userId){
+			UserInformation user;
+			switch (userId) {
+				case 1:
+					user = new UserInformation("Akshata", "Shanbhag", 28);
+					break;
+				case 2:
+					user = new UserInformation("Sam", "Khawase", 29);
+					break;
+				default:
+					return null;
+			}
+			return user;
+		}
+	}
+}
---
+++ SmartController/UserInformation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Runtime.Serialization;
+
+namespace MyNameSpace
+{
+	[DataContract()]
+	public class UserInformation
+	{
+
+#region " Constructors "
+	public UserInformation(string userFirstName, string userLastName, System.Nullable<System.Int32> age)
+	{
+		_firstName = userFirstName;
+		_lastName = userLastName;
+		_age = age;
+	}
+#endregion
+
+
+#region " Fields "
+	private System.String _firstName;
+	private System.String _lastName;
+	private System.Nullable<Int32> _age;
+#endregion
+
+#region " Properties "
+	[DataMember()]
+	public System.String UserName
+	{
+		[DebuggerStepThrough()]
+		get { return _firstName + " " + _lastName; }
+		[DebuggerStepThrough()]
+		set { _firstName = value; }
+	}
+
+	[DataMember()]
+	public System.Nullable<System.Int32> Age
+	{
+		[DebuggerStepThrough()]
+		get { return _age; }
+		[DebuggerStepThrough()]
+		set { _age = value; }
+	}
+#endregion
+
+	}
+}
